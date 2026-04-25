ca8ad85a0306f1d446bbd8960e8c21b38a61ea3e
Rapip <illiyyinrafif@gmail.com>
2026-04-25 22:28:18 +0700

    prepare api backend

---
+++ src/app/api/admin/verify-payment/route.ts
@@ -0,0 +1,6 @@
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { getErrorMessage } from "@/lib/errorMessage";
+
